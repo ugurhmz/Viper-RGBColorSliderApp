a3b46e1865bfd067d0d97e23052c718e9df14340
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-04-21 15:10:48 +0300

    UserDefaults current changed color added

--- Viper-RGBSliderApp/HomeVC.swift
+++ Viper-RGBSliderApp/HomeVC.swift
@@ -16,7 +16,19 @@ class HomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        
+        let rgb = loadCurrentColor()
+        print("kaydedilen \(rgb.0), \(rgb.1), \(rgb.2)")
+        sliderRed.value = Float(rgb.0)
+        sliderGreen.value = Float(rgb.1)
+        sliderBlue.value = Float(rgb.2)
+        
+        
+        rgbLabel.text = "RGB ( \(rgb.0), \(rgb.1), \(rgb.2) )"
+        
+        self.view.backgroundColor = UIColor(red: rgb.0/255,
+                                            green: rgb.1/255,
+                                            blue: rgb.2/255, alpha: 1)
       
     }
 
@@ -40,6 +52,26 @@ class HomeVC: UIViewController {
         onColorValueChange()
     }
     
+    
+    // save color
+    func saveCurrentColor(rgb: (CGFloat, CGFloat, CGFloat)) -> (Void) {
+        UserDefaults.standard.set(rgb.0, forKey: "Red")
+        UserDefaults.standard.set(rgb.1, forKey: "Green")
+        UserDefaults.standard.set(rgb.2, forKey: "Blue")
+        
+        print("\(rgb.0), \(rgb.1), \(rgb.2)")
+    }
+    
+    // load current color
+    func loadCurrentColor() -> (CGFloat, CGFloat, CGFloat) {
+        
+        let red = CGFloat(UserDefaults.standard.float(forKey: "Red"))
+        let green = CGFloat(UserDefaults.standard.float(forKey: "Green"))
+        let blue = CGFloat(UserDefaults.standard.float(forKey: "Blue"))
+        
+        return (red, green, blue)
+    }
+    
 }
 
 
@@ -58,6 +90,8 @@ extension HomeVC: HomeColorValueDelegateProtocol {
         rgbLabel.text = "RGB ( \(red), \(green), \(blue) )"
         
         self.view.backgroundColor = UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+        
+        saveCurrentColor(rgb: (red, green, blue))
     }
     
 }
