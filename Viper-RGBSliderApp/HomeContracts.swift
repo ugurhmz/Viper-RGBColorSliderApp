852aa5ab542d26ddb9be047d4b6a3e39d7c947c6
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-04-21 14:43:18 +0300

    View back ground changed with slider

---
+++ Viper-RGBSliderApp/HomeContracts.swift
@@ -0,0 +1,12 @@
+//
+//  HomeContracts.swift
+//  Viper-RGBSliderApp
+//
+//  Created by ugur-pc on 21.04.2022.
+//
+
+import Foundation
+
+protocol HomeColorValueDelegateProtocol {
+    func onColorValueChange()
+}
--- Viper-RGBSliderApp/HomeVC.swift
+++ Viper-RGBSliderApp/HomeVC.swift
@@ -17,24 +17,47 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
+      
     }
 
     
     
     @IBAction func onRedChanged(_ sender: UISlider) {
         print(sender.value)
+        onColorValueChange()
     }
     
     
 
     @IBAction func onGreenValueChanged(_ sender: UISlider) {
         print(sender.value )
+        onColorValueChange()
     }
     
     
     @IBAction func onBlueValueChanged(_ sender: UISlider) {
         print(sender.value)
+        onColorValueChange()
     }
     
 }
 
+
+//MARK: -
+extension HomeVC: HomeColorValueDelegateProtocol {
+    
+    
+    func onColorValueChange() {
+        
+        let step: Float = 1
+        
+        let red = CGFloat( round(sliderRed.value / step) * step)
+        let green = CGFloat(round(sliderGreen.value / step) * step)
+        let blue = CGFloat(round(sliderBlue.value / step) * step)
+        
+        rgbLabel.text = "RGB ( \(red), \(green), \(blue) )"
+        
+        self.view.backgroundColor = UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
